fdf65ecb03329be5ea51f43ae7296e894c15421f
mikeeus <itsmikias@gmail.com>
2018-01-06 15:23:05 +0300

    Handle single arguments. Only accept xls and xlsx extensions. Clean up

--- xls_to_csv.py
+++ xls_to_csv.py
@@ -1,6 +1,7 @@
 import xlrd
 import csv
 import sys
+import string
 
 # Converts xls and xlsx spreadsheets from 'static/records/source/` to csv
 def csv_from_excel():
@@ -14,17 +15,24 @@ def csv_from_excel():
         return
 
     source_filename = sys.argv[1]
+    ext = source_filename.split('.')[1]
+    if ext != 'xls' and ext != 'xlsx':
+        print(string.Template("Error: expecting .xls or .xlsx file extension, but got '$ext'.").substitute(ext=ext))
+        return
 
+    # sheet name
     if args_length > 2:
         sheet_name = sys.argv[2]
-        if args_length > 3:
-            output_filename = sys.argv[3]
-        else:
-            ext = source_filename.split('.')[1]
-            output_filename = source_filename.replace(ext, 'csv')
     else:
         sheet_name = 'Sheet1'
 
+    # output filename
+    if args_length > 3:
+        output_filename = sys.argv[3]
+    else:
+        output_filename = source_filename.replace(ext, 'csv')
+
+    # open file and convert to csv
     workbook = xlrd.open_workbook(source_path + source_filename)
     sheet = workbook.sheet_by_name(sheet_name)
     csv_file = open(csv_path + output_filename, 'w')
