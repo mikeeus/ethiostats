f6552924d1f868e3fc12639db5dd1893d39d451b
mikeeus <itsmikias@gmail.com>
2018-01-06 14:45:12 +0300

    Update validations

--- src/forms/hscode_form.cr
+++ src/forms/hscode_form.cr
@@ -23,12 +23,9 @@ class HscodeForm < Hscode::BaseForm
     validate_required heading_id
     validate_required code
     validate_required description
-    validate_required unit
 
     validate_required duty
     validate_required excise
-    validate_required vat
     validate_required sur
-    validate_required withholding
   end
 end
