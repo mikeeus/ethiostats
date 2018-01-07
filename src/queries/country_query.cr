9920e74e2728dfca2e379536da399e1b43c7ebc2
mikeeus <itsmikias@gmail.com>
2018-01-07 10:03:38 +0300

    Replace old unique query with find_by_hash? queries, add country find_by_name? query

--- src/queries/country_query.cr
+++ src/queries/country_query.cr
@@ -1,2 +1,9 @@
 class CountryQuery < Country::BaseQuery
+  def find_by_name?(name : String)
+    LuckyRecord::Repo.run do |db|
+      _name = PG::EscapeHelper.escape_literal(name)
+      statement = "SELECT id, name, short, coordinates, created_at, updated_at FROM countries WHERE name ILIKE #{_name} OR #{_name} ILIKE ANY (aliases)"
+      db.query_one? statement, as: Country
+    end
+  end
 end
--- src/queries/export_query.cr
+++ src/queries/export_query.cr
@@ -1,6 +1,5 @@
 class ExportQuery < Export::BaseQuery
-  # Returns true if an export with the given properties is not found
-  def is_unique(hscode_id, year, month, cpc, destination_id, fob_etb, fob_usd)
-    where(hscode_id: hscode_id, year: year, month: month, cpc: cpc, destination_id: destination_id, fob_etb: fob_etb, fob_usd: fob_usd).first?
+  def find_by_hash?(hash)
+    unique_hash(hash).first?
   end
 end
