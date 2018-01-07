24f78ee8af2b8af36adebec23c0c196fec64347b
mikeeus <itsmikias@gmail.com>
2018-01-07 10:07:18 +0300

    create missing hscodes

--- src/lib/record_importer_helpers.cr
+++ src/lib/record_importer_helpers.cr
@@ -3,6 +3,7 @@ module RecordImporterHelpers
   @errors = [] of NamedTuple(msg: String, row: CSV)
 
   private def add_error(row, msg)
+    pp msg
     @errors << {
       msg: msg,
       row: row
@@ -47,5 +48,32 @@ module RecordImporterHelpers
 
   private def to_cents(value)
     (value.to_f * 100).to_i.to_s
+  end
+
+  def create_missing_hscode(row)
+    zero_section = SectionQuery.new.code("00").first
+    zero_chapter = ChapterQuery.new.code("0000").first
+    zero_heading = HeadingQuery.new.code("000000").first
+
+    hscode = HscodeForm.new(
+      section_id: zero_section.id.to_s,
+      chapter_id: zero_chapter.id.to_s,
+      heading_id: zero_heading.id.to_s,
+      code: row[@headers["code"]],
+      description: row[@headers["description"]],
+      unit: "UN",
+      duty: "0",
+      excise: "0",
+      vat: "15",
+      sur: "10",
+      withholding: "3",
+    )
+
+    hscode.save!
+    #   hscode
+    # else
+    #   pp hscode.errors
+    #   raise "Error creating missing hscode."
+    # end
   end
 end
