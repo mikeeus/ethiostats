a987722dc428814dcc40c278f6b706f0aea87de0
mikeeus <itsmikias@gmail.com>
2018-01-21 13:54:40 +0300

    Add favicons

--- src/pages/main_layout.cr
+++ src/pages/main_layout.cr
@@ -28,6 +28,7 @@ abstract class MainLayout
         css_link asset("css/datatable.min.css")
         c3_scripts_and_css
         csrf_meta_tags
+        favicons
         meta name: "viewport", content: "width=device-width, initial-scale=1"
       end
 
@@ -44,5 +45,15 @@ abstract class MainLayout
 
   def page_title
     "Ethiostats"
+  end
+
+  private def favicons
+    raw %(
+      <link rel="apple-touch-icon" sizes="76x76" href="/images/favicons/apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png">
+      <link rel="manifest" href="/images/favicons/manifest.json">
+      <link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    )
   end
 end
